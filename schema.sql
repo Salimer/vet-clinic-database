a564dfb5d9de284a83853d35b3b5a3e9f668cbdd
Salimer <52242629+Salimer@users.noreply.github.com>
2023-07-10 17:52:57 +0300

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,27 @@
+/* Database schema to keep the structure of entire database. */
+
+billingdb=# create database vet_clinic;
+CREATE DATABASE
+billingdb=# \c vet_clinic
+You are now connected to database "vet_clinic" as user "dragon10".
+vet_clinic=# CREATE TABLE ANIMALS(
+vet_clinic(# ID INT PRIMARY KEY NOT NULL,
+vet_clinic(# NAME VARCHAR NOT NULL,
+vet_clinic(# DATE_OF_BIRTH DATE NOT NULL,
+vet_clinic(# ESCAPE_ATTEMPTS INT NOT NULL,
+vet_clinic(# NEUTERED BOOL NOT NULL,
+vet_clinic(# WEIGHT_KG DECIMAL(3,2) NOT NULL,
+vet_clinic(# );
+ERROR:  syntax error at or near ")"
+LINE 8: );
+        ^
+vet_clinic=# CREATE TABLE ANIMALS (
+vet_clinic(#   ID INT PRIMARY KEY NOT NULL,
+vet_clinic(#   NAME VARCHAR(50) NOT NULL,
+vet_clinic(#   DATE_OF_BIRTH DATE NOT NULL,
+vet_clinic(#   ESCAPE_ATTEMPTS INT NOT NULL,
+vet_clinic(#   NEUTERED BOOL NOT NULL,
+vet_clinic(#   WEIGHT_KG DECIMAL(3, 2) NOT NULL
+vet_clinic(# );
+CREATE TABLE                                                                                                                                              CREATE TABLE
+
