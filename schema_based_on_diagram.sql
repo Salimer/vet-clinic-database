5adf3fbad3d5721034b3dd83a1fc8979b62d71a1
katfogy <katfogy@gmail.com>
2023-07-18 11:56:02 +0100

    invoice, treatments, invoice_items and treatment_history added

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -18,3 +18,22 @@ CREATE TABLE invoices(
     payed_at TIMESTAMP,
     medical_history_id INT UNIQUE REFERENCES medical_histories(id)
 );
+
+CREATE TABLE treatments(
+    id SERIAL PRIMARY KEY,
+    type VARCHAR,
+    name VARCHAR
+);
+CREATE TABLE invoice_items(
+    id SERIAL PRIMARY KEY,
+    unit_price DECIMAL(10, 2),
+    quantity INT,
+    total_price DECIMAL(10, 2),
+    invoice_id INT REFERENCES invoices(id),
+    treatment_id INT REFERENCES treatments(id)
+);
+CREATE TABLE treatments_histories(
+    medical_histories_id INT REFERENCES medical_histories(id),
+    treatments_id INT REFERENCES treatments(id),
+    PRIMARY KEY(medical_histories_id, treatments_id)
+);
